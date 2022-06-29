7509fcb295685968badf5f03308ba09613879a24
deepa <deepashah2511@gmail.com>
2022-06-29 14:57:50 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ThinkQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(  "Good Afternoon ...Welcomeee");
+        }
+    }
+}
