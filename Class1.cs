83aa66e605519b82d9962189f75e9ab750ceae54
deepa <deepashah2511@gmail.com>
2022-06-29 17:15:44 +0530

    second commit

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThinkQ
+{
+    class DemoThread
+    {
+        static void m1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i+Thread.CurrentThread.Name);
+               
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Afternoon ...Welcomeee");
+            Thread t1 = new Thread(m1);
+            Thread t2 = new Thread(m1);
+            t1.Name = "OM";
+            t2.Name = "Ram";
+            t1.Start();
+            t2.Start();
+            for(int i=11;i<=20;i++)
+            {
+                Console.WriteLine("main"+i);
+            }
+            Console.WriteLine("main ends");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThinkQ
 {
     class Program
-    {
+
+    { 
+        static void m1()
+        {
+            for(int i=1;i<=10;i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(2000);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(  "Good Afternoon ...Welcomeee");
+            m1();
+            Console.WriteLine("main ends");
         }
     }
 }
